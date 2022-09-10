604a25101a9c91f94b7ea595dd69c556cf1b8f8e
Andri Firman Saputra <andrifirmansaputra1@gmail.com>
2022-09-11 03:35:55 +0700

    update

--- semester 5/Pemrograman Web/Project Kelompok/modul_jepang/assets/js/modul_jepang.js
+++ semester 5/Pemrograman Web/Project Kelompok/modul_jepang/assets/js/modul_jepang.js
@@ -1,6 +1,38 @@
+// Transparent Navbar
+let nav = document.querySelector('#navbar');
+
+window.addEventListener('scroll', function() 
+{
+  if (window.pageYOffset > 50) 
+  {
+    nav.classList.add('bg-danger', 'shadow');
+  } 
+  else 
+  {
+    nav.classList.remove('bg-danger', 'shadow');
+  }
+});
+
+// Preloader
 let preloader = document.querySelector('#preloader');
 if (preloader) {
 	window.addEventListener('load', function() {
 		preloader.remove();
 	});
+}
+
+// Back to Top
+let backtotop = document.querySelector('.back-to-top');
+
+if (backtotop) {
+	const toggleBacktotop = function() {
+		if (window.scrollY > 100) {
+			backtotop.classList.add('active');
+		} else {
+			backtotop.classList.remove('active');
+		}
+	}
+	
+	window.addEventListener('load', toggleBacktotop);
+	window.addEventListener('scroll', toggleBacktotop);
 }
