7df0c0802ccf8e2a4d213c18d407fcdff2d865e1
minstrel975 <andrifirmansaputra1@gmail.com>
2022-10-01 10:26:31 +0700

    latihan 1

---
+++ semester 5/PEMROGRAMAN WEB 1/Project Kelompok/modul_jepang/assets/js/latihan1_modul_jepang.js
@@ -0,0 +1,198 @@
+//declare all variables
+var start = document.getElementById("start");
+var quiz = document.getElementById("quiz");
+var quizQuestion = document.getElementById("quizQuestion");
+var quizImg = document.getElementById("quizImg");
+var optionA = document.getElementById("choiceA");
+var optionB = document.getElementById("choiceB");
+var optionC = document.getElementById("choiceC");
+var optionD = document.getElementById("choiceD");
+var scoreBlock = document.getElementById("scoreBlock");
+var scoreMessage = document.getElementById("scoreMessage");
+var quizAgain = document.getElementById("quizAgain");
+var choices = document.getElementById("choices");
+var choiceResponse = document.getElementById("choiceResponse");
+var score = 0;
+
+//questions function so our getQuestion function later can get the right value from array
+
+let questions = [{
+    question: "Huruf apakah ini?",
+    imgSrc: "assets/img/materi/huruf/hiragana/a.png",
+    choiceA: "a",
+    choiceB: "u",
+    choiceC: "o",
+    choiceD: "ka",
+    correctAnswer: "A"
+}, {
+    question: "Huruf apakah ini?",
+    imgSrc: "assets/img/materi/huruf/hiragana/e.png",
+    choiceA: "o",
+    choiceB: "e",
+    choiceC: "u",
+    choiceD: "i",
+    correctAnswer: "B"
+}, {
+    question: "Which bird is this?",
+    imgSrc: "assets/birds/morepork.jpg",
+    choiceA: "Morepork/Ruru",
+    choiceB: "Takahē",
+    choiceC: "Wax-eye",
+    choiceD: "New Zealand Parakeet/Kākāriki",
+    correctAnswer: "A"
+}, {
+    question: "Which bird is this?",
+    imgSrc: "assets/birds/tui.jpg",
+    choiceA: "Tūī",
+    choiceB: "Whitehead/Pōpokotea",
+    choiceC: "Wrybill/Ngutu pare",
+    choiceD: "Weka",
+    correctAnswer: "A"
+}, {
+    question: "Which bird is this?",
+    imgSrc: "assets/birds/dotterel.jpg",
+    choiceA: "Westland Petrel/Tāiko",
+    choiceB: "Variable Oystercatcher/Tōrea",
+    choiceC: "Subantarctic Teal",
+    choiceD: "New Zealand Dotterel/Tūturiwhatu",
+    correctAnswer: "D"
+}, {
+    question: "Which bird is this?",
+    imgSrc: "assets/birds/kakapo.jpg",
+    choiceA: "Kākā",
+    choiceB: "Kea",
+    choiceC: "Kākāpō",
+    choiceD: "Kōkako",
+    correctAnswer: "C"
+}, {
+    question: "Which bird is this?",
+    imgSrc: "assets/birds/kotuku.jpg",
+    choiceA: "New Zealand Fairy Tern/Tara iti",
+    choiceB: "White Heron/Kōtuku",
+    choiceC: "Shore Plover/Tūturuatu",
+    choiceD: "Spotted Shag/Parekareka",
+    correctAnswer: "B"
+}, {
+    question: "Which bird is this?",
+    imgSrc: "assets/birds/kea.jpg",
+    choiceA: "New Zealand Robin/Toutouwai",
+    choiceB: "Paradise Duck / Pūtakitaki",
+    choiceC: "Pūkeko",
+    choiceD: "Kea",
+    correctAnswer: "D"
+}, {
+    question: "Which bird is this?",
+    imgSrc: "assets/birds/kaka.jpg",
+    choiceA: "Kākā",
+    choiceB: "Tomtit/Miromiro",
+    choiceC: "Yellowhead/Mohua",
+    choiceD: "Fiordland Crested Penguin/Tawaki",
+    correctAnswer: "A"
+}, {
+    question: "Which bird is this?",
+    imgSrc: "assets/birds/kereru.jpg",
+    choiceA: "Kiwi",
+    choiceB: "New Zealand Pigeon/Kererū",
+    choiceC: "Kingfisher/Kōtare",
+    choiceD: "Eastern Bar-Tailed Godwit/Kuaka",
+    correctAnswer: "B"
+}, ];
+
+
+var questionIndex = 0;
+
+
+// getQuestion function
+
+function getQuestion() {
+
+    choiceResponse.style.display = "none";
+    let q = questions[questionIndex];
+    quizQuestion.innerHTML = "<p>Soal " +(questionIndex+1) + ": " + q.question + "</p>";
+    quizImg.innerHTML = "<img src=" + q.imgSrc + ">";
+    optionA.innerHTML = q.choiceA;
+    optionB.innerHTML = q.choiceB;
+    optionC.innerHTML = q.choiceC;
+    optionD.innerHTML = q.choiceD;
+    choices.style.display = "block";
+}
+
+
+// start quiz
+
+function beginQuiz() {
+    $("#latihan1").removeClass("mt-5");
+    $('.hidden-start').hide();
+    start.style.display ="none";
+    getQuestion();
+    quiz.style.display = "block";
+}
+
+// show score function
+
+function showScore() {
+    $("#latihan1").addClass("mt-5");
+    quiz.style.display = "none";
+    scoreBlock.style.display = "block";
+    scoreBlock.innerHTML = "<h2> Skormu adalah " + score + " dari 10!</h2>";
+
+    if (score < 4) {
+        scoreMessage.innerHTML = "<p>Belajar lagi! Waktunya sedikit lebih serius.</p>";
+    }
+    else if (score < 8) {
+        scoreMessage.innerHTML = "<p>Lumayan! Sedikit belajar lagi, pasti bisa.</p>"
+    }
+    else {
+        scoreMessage.innerHTML = "<p>Pandai! Kamu telah belajar dengan sungguh-sungguh pertahanankan!</p>"
+    }
+    scoreMessage.style.display = "block";
+    quizAgain.style.display = "inline-block";
+}
+
+
+//function to check if answer is correct
+
+function check(answer) {
+    if (questionIndex < questions.length - 1) {
+        if (answer == questions[questionIndex].correctAnswer) {
+            score++;
+            questionIndex++;
+            choices.style.display = "none";
+            choiceResponse.innerHTML= "<p class='btn btn-success'>Benar!</p>"
+            choiceResponse.style.display = "block";
+            setTimeout(getQuestion,1500);
+        }
+        else {
+            questionIndex++;
+            choices.style.display = "none";
+            choiceResponse.innerHTML= "<p class='btn btn-danger'>Salah!</p>"
+            choiceResponse.style.display = "block";
+            setTimeout(getQuestion,1500);
+        }
+    }
+    else {
+        if (answer == questions[questionIndex].correctAnswer) {
+            score++;
+            choices.style.display = "none";
+            choiceResponse.innerHTML= "<p class='btn btn-success'>Benar!</p>"
+            choiceResponse.style.display = "block";
+            setTimeout(showScore,1500);
+        }
+        else {
+            choices.style.display = "none";
+            choiceResponse.innerHTML= "<p class='btn btn-danger'>Salah!</p>"
+            choiceResponse.style.display = "block";
+            setTimeout(showScore,1500);
+        }
+    }
+}
+
+function restartQuiz() {
+    $('.hidden-start').show();
+    start.style.display = "block";
+    scoreBlock.style.display = "none";
+    scoreMessage.style.display = "none";
+    quizAgain.style.display = "none";
+    score = 0;
+    questionIndex = 0;
+}
