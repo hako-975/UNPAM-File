6f45267b46501864f59211afc4b0e3af0bbba129
minstrel975 <andrifirmansaputra1@gmail.com>
2022-10-27 11:41:53 +0700

    baru

---
+++ Bekas/praktikum oop/Praktikum OOP/Source Code/2/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+       System.out.println("Kok Bisa ya?");
+    }
+}
+
+class TestLainnya {
+    public static void main(String[] args) {
+        System.out.println("Kok Bisa ya?");
+    }
+}
