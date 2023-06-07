923ed704869e3da6cbda2999f9ebdf774c992e56
Andri Firman Saputra <andrifirmansaputra1@gmail.com>
2023-06-07 13:58:16 +0700

    baru

--- semester 6/KECERDASAN BUATAN/python/c420.py
+++ semester 6/KECERDASAN BUATAN/python/c420.py
@@ -1,14 +1,17 @@
+from matplotlib import pyplot as plt
+
+
 class BaseFuzzy():
     def __init__(self):
         self.max = 0
         self.min = 0
-    
+
     def up(self, x):
         return (x - self.min) / (self.max - self.min)
-    
+
     def down(self, x):
         return (self.max - x) / (self.max - self.min)
-    
+
 
 class Temp(BaseFuzzy):
     def __init__(self):
@@ -16,7 +19,8 @@ class Temp(BaseFuzzy):
         self.t2 = 15
         self.t3 = 25
         self.t4 = 55
-    
+        self.tn = 60
+
     def freeze(self, x):
         # 0-t1 = 1
         # t1-t2 = down
@@ -28,7 +32,7 @@ class Temp(BaseFuzzy):
             return self.down(x)
         else:
             return 0
-        
+
     def cold(self, x):
         # t1-t2 = up
         # t2-t3 = down
@@ -42,7 +46,7 @@ class Temp(BaseFuzzy):
             return self.down(x)
         else:
             return 0
-    
+
     def warm(self, x):
         # t2-t3 = up
         # t3-t4 = down
@@ -56,7 +60,7 @@ class Temp(BaseFuzzy):
             return self.down(x)
         else:
             return 0
-    
+
     def hot(self, x):
         # t3-t4 = up
         # t4-... = 1
@@ -68,3 +72,42 @@ class Temp(BaseFuzzy):
             return 1
         else:
             return 0
+
+    def graph(self, value=None):
+        # freeze
+        # 0-t1 = 1 [1, 1]
+        # t1-t2 = down [1, 0]
+        # t2 -tn = 0 [0, 0]
+        x_freeze = [0, self.t1, self.t2]
+        y_freeze = [1, 1, 0]
+        freeze_value = self.freeze(value)
+        y_freeze_v = [freeze_value, freeze_value, 0]
+        plt.plot(x_param, y_freeze_v, label=('v_freeze[{freeze_value}]'))
+
+        plt.plot(x_freeze, y_freeze, label='freeze')
+
+        # cold
+        # t1-t2 = up
+        # t2-t3 = down
+
+        # warm
+        # t2-t3 = up
+        # t3-t4 = down
+
+        # hot
+        # t3-t4 = up
+        # t4-... = 1
+        plt.legend(loc='upper right')
+        plt.show()
+
+
+temp = Temp()
+
+x = 22
+
+print('freeze', temp.freeze(x))
+print('cold', temp.cold(x))
+print('warm', temp.warm(x))
+print('hot', temp.hot(x))
+
+temp.graph()
